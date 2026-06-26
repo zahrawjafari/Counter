ae6f80c7b8cd1c7ca5f40ca41a39f582abec8ed6
zahrawjafari <zahra.jafari8112@gmail.com>
2026-06-26 23:02:42 +0330

    z

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 let count = 0;
-const min = document.getElementById(min);
+const number = document.getElementById("count");
+const plus =
+document.getElementById("plus");
+const min =
+document.getElementById("min");
+const reset = 
+document.getElementById("reset");
+plus.addEventListener("click" , () => { count++;
+    number.textContent = count;
+});
+
+min.addEventListener("click", () => {
+  count--;
+    number.textContent = count;
+});
+reset.addEventListener("click", () => {
+  count = 0;
+    number.textContent = count;})
+
+
 
