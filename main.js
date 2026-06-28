e7306a8780113d7e59b449fab5eee09152b37a55
zahrawjafari <zahra.jafari8112@gmail.com>
2026-06-28 20:52:40 +0330

    z

--- main.js
+++ main.js
@@ -6,17 +6,30 @@ const min =
 document.getElementById("min");
 const reset = 
 document.getElementById("reset");
+function updatecolor() {
+  if(count>0) {
+    number.style.color ="green";
+  } else if (count < 0) {
+    number.style.color ="red";
+  }else if ("black") {
+    number.style.color ="black"; 
+}
+}
 plus.addEventListener("click" , () => { count++;
     number.textContent = count;
+    updatecolor();
 });
 
 min.addEventListener("click", () => {
   count--;
     number.textContent = count;
+     updatecolor();
 });
 reset.addEventListener("click", () => {
   count = 0;
-    number.textContent = count;})
+    number.textContent = count;
+     updatecolor();
+  })
 
 
 
